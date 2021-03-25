16d06de7f68ea8278126c91f0d28bc0c1a07b8a8
mathru <user@mathru.net>
2021-03-25 18:51:01 +0900

    Durationのformatを追加

--- lib/src/extensions.dart
+++ lib/src/extensions.dart
@@ -535,6 +535,21 @@ extension RandomExtension on Random {
       ((nextDouble() * (max - min)) + min).limit(min, max);
 }
 
+extension DurationExtension on Duration {
+  /// Format and output the duration.
+  ///
+  /// Enter the format of the date and time in [format].
+  String format(String format) {
+    assert(format.isNotEmpty, "The format is empty.");
+    String twoDigits(int n) => n.toString().padLeft(2, "0");
+    return format
+        .replaceAll("dd", inDays.toString())
+        .replaceAll("HH", twoDigits(inHours))
+        .replaceAll("mm", twoDigits(inMinutes.remainder(60)))
+        .replaceAll("ss", twoDigits(inSeconds.remainder(60)));
+  }
+}
+
 extension DateTimeExtension on DateTime {
   /// Gets the localized week.
   String get localizedWeekDay => "WeekDay${weekday.toString()}".localize();
